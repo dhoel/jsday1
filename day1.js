8c669eb0cc9fb2bb4d70add849a22f6f3b2f2433
Dave Hoel <dkhoel@gmail.com>
2017-01-30 13:05:38 -0700

    added calcYOB func

--- day1.js
+++ day1.js
@@ -1,6 +1,10 @@
 function whoAmI(name, age) {
-  var yearOfBirth = 2017 - age;
+  var yearOfBirth = calcYOB(age);
   console.log("my name is " + name  + " and I'm "+ age + " years young");
   console.log("I was born in " + yearOfBirth +".");
 }
+
+function calcYOB(age) {
+  return 2017 - age;
+  }
 whoAmI("aaron", 39);
