49b5b0ec1995a496740a78c0e6618ff898cf474d
Jean-Luc19 <aticus19@gmail.com>
2017-01-30 13:42:58 -0700

    added more checks to validate parameters

--- day1.js
+++ day1.js
@@ -1,19 +1,29 @@
 function whoAmI(name, age) {
-  var yearOfBirth = calcYOB(age);
-  console.log("my name is " + name  + " and I'm "+ age + " years young");
-  console.log("I was born in " + yearOfBirth +".");
+    var yearOfBirth = calcYOB(age);
+    console.log("my name is " + name + " and I'm " + age + " years young");
+    console.log("I was born in " + yearOfBirth + ".");
 }
 
 function calcYOB(age) {
-try {
-
-  if (age < 1) {
-    throw new Error("Age must be a positive value.");
-    return 2017 - age;
-  }
-} catch (e) {
-  console.log('Something is wrong: ' + e.message);
+    try {
+      if (typeof(age) == 'undefined') {
+        throw new Error ("age hasn't been defined");
+      }
+      if (typeof(name) == 'undefined'){
+        throw new Error ("Name not defined")
+      }
+        if (age < 1) {
+            throw new Error("Age must be a positive value.");
+            return 2017 - age;
+        }
+        if (isNaN(age)) {
+          throw new Error ("put in a number dude")
+        }
+        return 2017 - age;
 }
+     catch (e) {
+        console.log('Something is wrong: ' + e.message);
+    }
 }
 
-whoAmI("aaron", 39);
+whoAmI(null, 23);
